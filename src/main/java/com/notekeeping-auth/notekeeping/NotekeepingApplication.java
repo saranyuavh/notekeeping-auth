f6797339323bb715339aef6009226dedd9253e65
saranyuavh <saranyu38@gmail.com>
2025-08-14 00:17:01 -0400

    renaming done

---
+++ src/main/java/com/notekeeping-auth/notekeeping/NotekeepingApplication.java
@@ -0,0 +1,13 @@
+package com.notekeeping.notekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotekeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotekeepingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notekeeping-auth/notekeeping/NotekeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notekeeping.notekeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotekeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
